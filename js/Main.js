21ee83085ccb968e3da227e3ec87b19b38949d12
Narayanan Chandran <narayanan@seriousandroiddeveloper.in>
2015-02-07 23:09:46 +0530

    HTML support added

--- js/Main.js
+++ js/Main.js
@@ -8,6 +8,7 @@ Main.prototype.init = function(){
 	var localObj = this;
 	//console.log(localObj.graphDiv);
 	localObj.graphObj = localObj.addMxGraphcontainer(localObj.graphDiv);
+	localObj.overrideMethods();
 	localObj.loadgraph();
 };
 
@@ -20,6 +21,7 @@ Main.prototype.loadgraph = function(){
 	var localObj = this;
 	var parent = localObj.graphObj.getDefaultParent();
 	var prefix = 'shape=image;image=src/images/heart.png;';
+	localObj.graphObj.setConnectable(true);
 	localObj.graphObj.getModel().beginUpdate();
 	try{
 		localObj.graphObj.insertVertex(parent, "service", 'Bottom', 60, 60, 60, 60,
@@ -28,11 +30,35 @@ Main.prototype.loadgraph = function(){
 					prefix+'verticalLabelPosition=top;verticalAlign=bottom');
 		localObj.graphObj.insertVertex(parent, null, 'Left', 60, 160, 60, 60,
 					prefix+'labelPosition=left;align=right');
-		localObj.graphObj.insertVertex(parent, null, 'Right', 140, 160, 60, 60,
-					prefix+'labelPosition=right;align=left');
+		localObj.graphObj.insertVertex(parent, "service2", '', 140, 160, 60, 60);
 	}
 	finally
 	{
 		localObj.graphObj.getModel().endUpdate();
 	}
+};
+
+Main.prototype.overrideMethods = function(){
+	var localObj = this;
+	localObj.graphObj.setHtmlLabels(true);
+	
+	localObj.graphObj.isCellEditable = function(cell)
+	{
+		return !localObj.graphObj.getModel().isEdge(cell);
+	};
+	
+	var getLabel2 = localObj.graphObj.getLabel;
+	localObj.graphObj.getLabel = function(cell){
+		if (localObj.graphObj.getModel().isVertex(cell)){
+			if(cell.id=="service2"){
+				return '<div style="height:60px;width:60px;background: url(\'src/images/heart.png\');"></div>';
+				//background: url("editors/images/grid.gif")
+			}else{
+				getLabel2.apply(this, arguments);
+			}
+		}else{
+			getLabel2.apply(this, arguments);
+		}
+	}
+	
 };
