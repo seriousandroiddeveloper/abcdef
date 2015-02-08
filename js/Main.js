c9c3eeba6a69b5d23eb0dc4e9efe99a6ab5ad00d
Narayanan Chandran <narayanan@seriousandroiddeveloper.in>
2015-02-08 17:31:47 +0530

    added Label and removed borders

--- js/Main.js
+++ js/Main.js
@@ -8,6 +8,11 @@ Main.prototype.init = function(){
 	var localObj = this;
 	//console.log(localObj.graphDiv);
 	localObj.graphObj = localObj.addMxGraphcontainer(localObj.graphDiv);
+	localObj.graphObj .stylesheet.getDefaultVertexStyle()[mxConstants.STYLE_FILLCOLOR] = 'white';
+	localObj.graphObj .stylesheet.getDefaultVertexStyle()[mxConstants.OUTLINE_COLOR] = 'white';
+	localObj.graphObj .stylesheet.getDefaultVertexStyle()[mxConstants.STYLE_STROKECOLOR] = 'white';
+	
+	
 	localObj.overrideMethods();
 	localObj.loadgraph();
 };
@@ -21,22 +26,51 @@ Main.prototype.loadgraph = function(){
 	var localObj = this;
 	var parent = localObj.graphObj.getDefaultParent();
 	var prefix = 'shape=image;image=src/images/heart.png;';
-	localObj.graphObj.setConnectable(true);
+	localObj.graphObj.setConnectable(false);
+	localObj.graphObj.setCellsResizable(false);
+	localObj.graphObj.setCellsMovable(false);
+	localObj.graphObj.setEnabled(false);
+	
 	localObj.graphObj.getModel().beginUpdate();
 	try{
 		var v1 = localObj.graphObj.insertVertex(parent, "service", 'Bottom', 60, 60, 60, 60,
 				prefix+'verticalLabelPosition=bottom;verticalAlign=top');
-		var v2 = localObj.graphObj.insertVertex(parent, "43", 'Top', 140, 60, 60, 60,
+		var v2 = localObj.graphObj.insertVertex(parent, "43", 'Top', 240, 60, 60, 60,
 					prefix+'verticalLabelPosition=top;verticalAlign=bottom');
 		var v3 = localObj.graphObj.insertVertex(parent, null, 'Left', 60, 160, 60, 60,
 					prefix+'labelPosition=left;align=right');
 		var v4 = localObj.graphObj.insertVertex(parent, "service2", '', 140, 160, 60, 60);
-		localObj.graphObj.insertEdge(parent, null, '', v1, v2);
+		//localObj.graphObj.insertEdge(parent, null, '', v1, v2);
 	}
 	finally
 	{
 		localObj.graphObj.getModel().endUpdate();
 	}
+	
+	document.body.appendChild(mxUtils.button('Morph', function()
+				{
+					localObj.graphObj.clearSelection();
+					
+					localObj.graphObj.getModel().beginUpdate();
+					try
+					{
+						localObj.graphObj.insertEdge(parent, null, '', v1, v2);
+					}
+					finally
+					{
+						// Arguments are number of steps, ease and delay
+						var morph = new mxMorphing(localObj.graphObj, 20, 1.2, 20);
+						morph.addListener(mxEvent.DONE, function()
+						{
+							localObj.graphObj.getModel().endUpdate();
+						});
+						morph.startAnimation();
+					}
+					
+					
+				}));
+	
+	
 };
 
 Main.prototype.overrideMethods = function(){
@@ -52,7 +86,7 @@ Main.prototype.overrideMethods = function(){
 	localObj.graphObj.getLabel = function(cell){
 		if (localObj.graphObj.getModel().isVertex(cell)){
 			if(cell.id=="service2"){
-				return '<div style="height:60px;width:60px;background: url(\'src/images/heart.png\');"></div>';
+				return '<div style="height:60px;width:60px;background: url(\'src/images/heart.png\');"></div><br>Service Provider ';
 				//background: url("editors/images/grid.gif")
 			}else{
 				getLabel2.apply(this, arguments);
